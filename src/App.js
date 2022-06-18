e3c1e9383cf292530796683c6d210354f691f1af
Fasih <fasihislam1999@gmail.com>
2022-06-18 15:48:22 +0400

    Fixed values in header

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ function App() {
  
 
   <div class="ui grid">
-  <div class="eight wide column">
+  <div class="ten wide column">
     <div class="container text-left">
-      <h1>Online Store</h1>      
+      <h1>Donate Today</h1>      
       <p>Mission, Vission & Values</p>
     </div>
   </div>
-  <div class="eight wide column"></div>
+  <div class="six wide column">
+  <div class="container text-left">
+      <h2>Donate By your choice</h2>      
+      <p>Accepting Dontation is Every Province</p>
+      <a id="bookmarkme" href="#" rel="sidebar" title="bookmark this page">Bookmark This Page</a>
+    </div>
+  </div>
 </div>
 </div>
 
@@ -28,14 +34,14 @@ function App() {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#"></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Deals</a></li>
-        <li><a href="#">Stores</a></li>
+        <li><a href="#">Why Donate</a></li>
+        <li><a href="#">Donate Today</a></li>
+        <li><a href="#">FAQ</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
