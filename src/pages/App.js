896a9a20c318e1a190b829953d962a865a55369d
Fasih <fasihislam1999@gmail.com>
2022-06-18 16:05:15 +0400

    made footer.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import Footer from './Footer';
 
 import '../css/App.css';
 
@@ -59,13 +60,8 @@ function App() {
   </div>
 </div><br /><br />
 
-<footer class="container-fluid text-center">
-  <p>Online Store Copyright</p>  
-  <form class="form-inline">Get deals:
-    <input type="email" class="form-control" size="50" placeholder="Email Address" />
-    <button type="button" class="btn btn-danger">Sign Up</button>
-  </form>
-</footer>
+
+    <Footer />    
     </div>
   );
 }
