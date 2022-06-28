5bbacfbecd9de3f46bedc96f8cfdc3b8572ec745
Fasih <fasihislam1999@gmail.com>
2022-06-28 18:02:40 +0400

    updated conaact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,6 +7,23 @@ const Contact = () => {
     return (
         <div>
             <Header />
+            <div className='main body ui container'>
+                <p className='red-heading' style={{margin: "0px"}}>Contact Us</p>
+                <p className='red-heading' style={{paddingTop: "0px"}}>Donate a Car to the Charity of Your Choice</p>
+                <div>
+                <p>Accepting Vehicle Donations in Every Province</p>
+                <p style={{wordSpacing: "4px"}}>
+                    Keeping our overhead low is important as it ensures a maximum donation amount for your charity. Would you consider connecting with us via our online submission form? Simply click the “Donate” button below! Thank you!
+                    <br></br><br></br>
+                    Toll Free: 1-1234-1234
+                    <br></br>
+                    Phone: 123-123-1234
+                    <br></br>
+                    Fax: 1-123-123-123
+                    <br></br>
+                    Mailing Address: 123 Hudson Bay Drive
+                </p>
+                </div> 
                 <br />
 
                 <div>
